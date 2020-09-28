26bdb6eb2d9b043bfc7a67b76e065b438c725d47
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-09-28 22:56:05 +0530

    Extracts header widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:covid19_flutter_app/styles/styles.dart';
 import 'package:covid19_flutter_app/utils/constant.dart';
-import 'package:covid19_flutter_app/utils/customClipper.dart';
 import 'package:covid19_flutter_app/widgets/counter.dart';
+import 'package:covid19_flutter_app/widgets/header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/screenutil.dart';
 import 'package:flutter_svg/svg.dart';
@@ -213,85 +213,6 @@ class HomeScreen extends StatelessWidget {
                 ],
               ),
             ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class Header extends StatelessWidget {
-  final String image;
-  final String textTop;
-  final String textBottom;
-
-  const Header({
-    Key key,
-    this.image,
-    this.textTop,
-    this.textBottom,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ClipPath(
-      clipper: HomeImageClipper(),
-      child: Container(
-        padding: EdgeInsets.only(
-          left: ScreenUtil().setHeight(100),
-          top: ScreenUtil().setHeight(100),
-          right: ScreenUtil().setHeight(60),
-        ),
-        height: ScreenUtil().setHeight(720),
-        width: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: [
-              Color(0xff3383cd),
-              Color(0xff11249f),
-            ],
-          ),
-          image: DecorationImage(
-            image: AssetImage('assets/images/virus.png'),
-          ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Align(
-              alignment: Alignment.topRight,
-              child: SvgPicture.asset('assets/icons/menu.svg'),
-            ),
-            SizedBox(
-              height: ScreenUtil().setHeight(40),
-            ),
-            Expanded(
-                child: Stack(
-              fit: StackFit.expand,
-              children: <Widget>[
-                Positioned(
-                  top: ScreenUtil().setHeight(20),
-                  left: ScreenUtil().setHeight(20),
-                  child: SvgPicture.asset(
-                    image,
-                    width: ScreenUtil().setHeight(460),
-                    alignment: Alignment.topCenter,
-                  ),
-                ),
-                Positioned(
-                  top: ScreenUtil().setHeight(60),
-                  left: ScreenUtil().setHeight(300),
-                  child: Text(
-                    '$textTop \n$textBottom',
-                    style: headingTextStyle.copyWith(
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ],
-            ))
           ],
         ),
       ),
