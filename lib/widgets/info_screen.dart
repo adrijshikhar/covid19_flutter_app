3486913443945dbac49637b725cfde8c60660a85
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-09-29 11:34:09 +0530

    Adds prevention card

--- lib/widgets/info_screen.dart
+++ lib/widgets/info_screen.dart
@@ -1,10 +1,9 @@
-import 'dart:developer';
-
 import 'package:covid19_flutter_app/styles/styles.dart';
 import 'package:covid19_flutter_app/utils/constant.dart';
 import 'package:covid19_flutter_app/widgets/header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 
 class InfoScreen extends StatelessWidget {
   @override
@@ -15,55 +14,139 @@ class InfoScreen extends StatelessWidget {
       height: 1920,
     );
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Header(
-            image: 'assets/icons/coronadr.svg',
-            textTop: "Get to know",
-            textBottom: "About Covid-19",
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  "Symptoms",
-                  style: titleTextstyle,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Header(
+              image: 'assets/icons/coronadr.svg',
+              textTop: "Get to know",
+              textBottom: "About Covid-19",
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Symptoms",
+                    style: titleTextstyle,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      SymptomCard(
+                        image: "assets/images/headache.png",
+                        title: "Headache",
+                        isActive: true,
+                      ),
+                      SymptomCard(
+                        image: "assets/images/caugh.png",
+                        title: "Caugh",
+                      ),
+                      SymptomCard(
+                        image: "assets/images/fever.png",
+                        title: "Fever",
+                      )
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    "Prevention",
+                    style: titleTextstyle,
+                  ),
+                  SizedBox(height: 20),
+                  PreventCard(
+                    image: "assets/images/wear_mask.png",
+                    title: "Wear face mask",
+                    text:
+                        "Since the start of coronavirus some palaces have fully embraced wearing facemasks",
+                  ),
+                  PreventCard(
+                    image: "assets/images/wash_hands.png",
+                    title: "Wash your hands",
+                    text:
+                        "Since the start of coronavirus some palaces have fully embraced wearing facemasks",
+                  ),
+                  SizedBox(height: 50),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PreventCard extends StatelessWidget {
+  final String image;
+  final String title;
+  final String text;
+  const PreventCard({
+    Key key,
+    this.image,
+    this.title,
+    this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: SizedBox(
+        height: 156,
+        child: Stack(
+          alignment: Alignment.centerLeft,
+          children: [
+            Container(
+              height: 136,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 8),
+                    blurRadius: 24,
+                    color: shadowColor,
+                  ),
+                ],
+              ),
+            ),
+            Image.asset(image),
+            Positioned(
+              left: 130,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                height: 136,
+                width: MediaQuery.of(context).size.width - 170,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    SymptomCard(
-                      image: "assets/images/headache.png",
-                      title: "Headache",
-                      isActive: true,
+                  children: [
+                    Text(
+                      title,
+                      style: titleTextstyle.copyWith(fontSize: 16),
                     ),
-                    SymptomCard(
-                      image: "assets/images/caugh.png",
-                      title: "Caugh",
+                    Text(
+                      text,
+                      style: TextStyle(
+                        fontSize: 12,
+                      ),
                     ),
-                    SymptomCard(
-                      image: "assets/images/fever.png",
-                      title: "Fever",
-                    )
+                    Align(
+                        alignment: Alignment.topRight,
+                        child: SvgPicture.asset("assets/icons/forward.svg"))
                   ],
                 ),
-                SizedBox(height: 20),
-                Text(
-                  "Prevention",
-                  style: titleTextstyle,
-                ),
-                SizedBox(height: 20),
-                
-              ],
+              ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
